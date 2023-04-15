9f65446f7367a908b74abe68bb24940819df9dfc
andreasonny83 <andreasonny83@gmail.com>
2023-04-15 17:04:35 +0100

    Update Jest config

--- jest.config.ts
+++ jest.config.ts
@@ -14,6 +14,23 @@ const customJestConfig = {
     '^@/app/(.*)$': '<rootDir>/app/$1',
     '^@/lib/(.*)$': '<rootDir>/lib/$1',
   },
+  collectCoverageFrom: [
+    './lib/**/*.tsx',
+    './components/**/*.tsx',
+    './app/**/*.tsx',
+    './lib/**/*.ts',
+    './components/**/*.ts',
+    './app/**/*.ts',
+  ],
+  coverageThreshold: {
+    global: {
+      statements: 100,
+      branches: 100,
+      functions: 100,
+      lines: 100,
+    },
+  },
+  resetMocks: true,
 };
 
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
