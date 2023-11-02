726e974f8c3c013a5e5bff6384f23513d5f6a1ec
andreasonny83 <andreasonny83@gmail.com>
2023-11-02 17:42:27 +0000

    test cache policy

--- app/(site)/cards/[cardId]/page.tsx
+++ app/(site)/cards/[cardId]/page.tsx
@@ -5,8 +5,8 @@ import { VariationEnum } from '@/components/types';
 import { Card } from '@/components/Card';
 import { logHelper } from '@/lib/helpers/log';
 
-export const dynamic = 'force-dynamic';
-export const revalidate = 0;
+// export const dynamic = 'force-dynamic';
+// export const revalidate = 0;
 
 export default async function Page({ params }: { params: { cardId: string } }) {
   const { data } = await getCardData(params.cardId);
